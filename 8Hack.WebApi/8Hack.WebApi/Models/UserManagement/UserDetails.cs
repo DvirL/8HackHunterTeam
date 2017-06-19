6e8d8846e2a64e2f41cc4b576525596f1c57ca59
Nadav Barami <nadavbara@gmail.com>
2017-06-20 00:56:51 +0300

    bla

--- 8Hack.WebApi/8Hack.WebApi/Models/UserManagement/UserDetails.cs
+++ 8Hack.WebApi/8Hack.WebApi/Models/UserManagement/UserDetails.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace _8Hack.WebApi.Models.UserManagement
 {
     public class UserDetails
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("id")]
         public string Id { get; set; }
     }
 }
